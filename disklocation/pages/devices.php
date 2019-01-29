45f02fd23f9e20e021ea5ea6f24235140e22a890
Ole-Henrik Jakobsen <ole@gidderikke.no>
2019-01-30 00:32:10 +0100

    Reuploaded files at correct location

--- disklocation/pages/devices.php
+++ disklocation/pages/devices.php
@@ -16,14 +16,24 @@
 		$tray_assign = ( empty($data["tray"]) ? $i : $data["tray"] );
 		
 		if(!$data) {
+			if($displayinfo["tray"]) {
+				$empty_tray = "<b>" . $tray_assign . "</b>" . $insert_break . "";
+			}
+			if($displayinfo["leddiskop"]) {
+				$empty_leddiskop = "<span class=\"grey-off\" alt=\"" . get_unraid_disk_status("grey-off", "DISK_NP") . "\" title=\"" . get_unraid_disk_status("grey-off", "DISK_NP") . "\" />&#11044;</span>" . $insert_break . "";
+			}
+			if($displayinfo["ledsmart"]) {
+				$empty_ledsmart = "<span class=\"grey-off\" alt=\"" . get_unraid_disk_status("grey-off", "DISK_NP") . "\" title=\"" . get_unraid_disk_status("grey-off", "DISK_NP") . "\" />&#11044;</span>";
+			}
+		
 			$disklocation_page .= "
 				<div style=\"order: " . $tray_assign . "\">
 					<div class=\"flex-container\">
 						<div style=\"background-color: #" . $color_array["empty"] . ";\">
 							<div class=\"flex-container-start\">
-								<b>" . $tray_assign . "</b>" . $insert_break . "
-								<span class=\"grey-off\" alt=\"" . get_unraid_disk_status("grey-off", "DISK_NP") . "\" title=\"" . get_unraid_disk_status("grey-off", "DISK_NP") . "\" />&#11044;</span>" . $insert_break . "
-								<span class=\"grey-off\" alt=\"" . get_unraid_disk_status("grey-off", "DISK_NP") . "\" title=\"" . get_unraid_disk_status("grey-off", "DISK_NP") . "\" />&#11044;</span>
+								$empty_tray
+								$empty_leddiskop
+								$empty_ledsmart
 							</div>
 							<div class=\"flex-container-middle\">
 								<b>Available disk slot</b>
@@ -48,105 +58,159 @@
 			$i_empty++;
 		}
 		else {
-			
-			$tray_assign = ( empty($data["tray"]) ? $i : $data["tray"] );
 			$device = $data["device"];
 			$devicenode = $data["devicenode"];
 			$luname = $data["luname"];
-			$smart_status = $data["smart_status"];
-			$smart_modelfamily = $data["model_family"];
-			$smart_modelname = $data["model_name"];
-			$smart_serialnumber = ( isset($data["smart_serialnumber"]) ? "(" . $data["smart_serialnumber"] . ")" : null );
-			$smart_temperature = ( isset($data["smart_temperature"]) ? $data["smart_temperature"] . "°C" : null );
-			$smart_powerontime = ( isset($data["smart_powerontime"]) ? "<span style=\"cursor: help;\" title=\"" . seconds_to_time($data["smart_powerontime"] * 60 * 60) . "\">" . $data["smart_powerontime"] . "h</span>" : null );
-			$smart_loadcycle = ( empty($data["smart_loadcycle"]) ? null : $data["smart_loadcycle"] . "c" );
-			$smart_capacity = human_filesize($data["smart_capacity"], 1, true);
 			
-			switch($data["smart_rotation"]) {
-				case -1:
-					$smart_rotation = "SSD";
-					break;
-				case 0:
-					$smart_rotation = "";
-					break;
-				default:
-					$smart_rotation = $data["smart_rotation"] . "rpm";
+			if($displayinfo["path"]) {
+				$device_page = $device;
+			}
+			if($displayinfo["devicenode"]) {
+				$devicenode_page = $devicenode;
+			}
+			if($displayinfo["luname"]) {
+				$luname_page = "(" . $luname . ")";
+			}
+			if($displayinfo["manufacturer"]) {
+				$smart_modelfamily = $data["model_family"];
+			}
+			if($displayinfo["devicemodel"]) {
+				$smart_modelname = $data["model_name"];
+			}
+			if($displayinfo["serialnumber"]) {
+				$smart_serialnumber = ( isset($data["smart_serialnumber"]) ? "<span style=\"white-space: nowrap;\">(" . $data["smart_serialnumber"] . ")</span>" : null );
+			}
+			if($displayinfo["powerontime"]) {
+				$smart_powerontime = ( isset($data["smart_powerontime"]) ? "<span style=\"cursor: help;\" title=\"" . seconds_to_time($data["smart_powerontime"] * 60 * 60) . "\">" . $data["smart_powerontime"] . "h</span>" : null );
+			}
+			if($displayinfo["loadcyclecount"]) {
+				$smart_loadcycle = ( empty($data["smart_loadcycle"]) ? null : $data["smart_loadcycle"] . "c" );
+			}
+			if($displayinfo["capacity"]) {
+				$smart_capacity = human_filesize($data["smart_capacity"], 1, true);
+			}
+			if($displayinfo["temperature"]) {
+				switch($tempunit) {
+					case 'F':
+						$smart_temperature = round(temperature_conv($data["smart_temperature"], 'C', 'F')) . "°F";
+						break;
+					case 'K':
+						$smart_temperature = round(temperature_conv($data["smart_temperature"], 'C', 'K')) . "K";
+						break;
+					default:
+						$smart_temperature = $data["smart_temperature"] . "°C";
+				}
+			}
+			if($displayinfo["rotation"]) {
+				switch($data["smart_rotation"]) {
+					case -1:
+						$smart_rotation = "SSD";
+						break;
+					case 0:
+						$smart_rotation = "";
+						break;
+					default:
+						$smart_rotation = $data["smart_rotation"] . "rpm";
+				}
+			}
+			if($displayinfo["formfactor"]) {
+				$smart_formfactor = str_replace(" inches", "&quot;", $data["smart_formfactor"]);
 			}
 			
-			$smart_formfactor = str_replace(" inches", "&quot;", $data["smart_formfactor"]);
-			
-			if($unraid_array[$devicenode]["color"] && $unraid_array[$devicenode]["status"]) {
-				if($unraid_array[$devicenode]["type"] == "Cache") {
-					$disk_status_type = "cache";
+			if($displayinfo["leddiskop"]) {
+				if($unraid_array[$devicenode]["color"] && $unraid_array[$devicenode]["status"]) {
+					if($unraid_array[$devicenode]["type"] == "Cache") {
+						$disk_status_type = "cache";
+					}
+					else {
+						$disk_status_type = "";
+					}
+					
+					$unraid_disk_status_message = get_unraid_disk_status($unraid_array[$devicenode]["color"], $unraid_array[$devicenode]["status"], $disk_status_type);
+					
+					if($unraid_array[$devicenode]["color"] == "green-blink") { $unraid_add_greenblinkid = " class=\"greenblink\" id=\"greenblink\""; } else { $unraid_add_greenblinkid = ""; }
+					$unraid_array_icon = "<span class=\"" . $unraid_array[$devicenode]["color"] . "\" alt=\"" . $unraid_disk_status_message . "\" title=\"" . $unraid_disk_status_message . "\" />&#11044;</span>";
 				}
 				else {
-					$disk_status_type = "";
+					usleep($smart_exec_delay . 000); // delay script to get the output of the next shell_exec()
+					$smart_powermode = trim(shell_exec("smartctl -n standby /dev/bsg/$device | grep Device"));
+					
+					if(strstr($smart_powermode, "ACTIVE")) {
+						$unraid_disk_status_color = "green-on";
+						$unraid_disk_status_message = get_unraid_disk_status($unraid_disk_status_color, "DISK_OK");
+						$unraid_add_greenblinkid = "";
+					}
+					else if(strstr($smart_powermode, "IDLE")) {
+						$unraid_disk_status_color = "green-on";
+						$unraid_disk_status_message = get_unraid_disk_status($unraid_disk_status_color, "DISK_OK");
+						$unraid_add_greenblinkid = "";
+					}
+					else if(strstr($smart_powermode, "STANDBY")) {
+						$unraid_disk_status_color = "green-blink";
+						$unraid_disk_status_message = get_unraid_disk_status($unraid_disk_status_color, "DISK_OK");
+						$unraid_add_greenblinkid = " class=\"greenblink\" id=\"greenblink\"";
+					}
+					else {
+						$unraid_disk_status_color = "grey-off";
+						$unraid_disk_status_message = get_unraid_disk_status($unraid_disk_status_color, "DISK_NP");
+						$unraid_add_greenblinkid = "";
+					}
+					
+					$unraid_array_icon = "<span class=\"" . $unraid_disk_status_color . "\" alt=\"" . $unraid_disk_status_message . "\" title=\"" . $unraid_disk_status_message . "\" />&#11044;</span>" . $insert_break . "";
 				}
-				
-				$unraid_disk_status_message = get_unraid_disk_status($unraid_array[$devicenode]["color"], $unraid_array[$devicenode]["status"], $disk_status_type);
-				
-				if($unraid_array[$devicenode]["color"] == "green-blink") { $unraid_add_greenblinkid = " class=\"greenblink\" id=\"greenblink\""; } else { $unraid_add_greenblinkid = ""; }
-				$unraid_array_icon = "<span class=\"" . $unraid_array[$devicenode]["color"] . "\" alt=\"" . $unraid_disk_status_message . "\" title=\"" . $unraid_disk_status_message . "\" />&#11044;</span>";
-			}
-			else {
-				usleep($smart_exec_delay . 000); // delay script to get the output of the next shell_exec()
-				$smart_powermode = trim(shell_exec("smartctl -n standby /dev/bsg/$device | grep Device"));
-				
-				if(strstr($smart_powermode, "ACTIVE")) {
-					$unraid_disk_status_color = "green-on";
-					$unraid_disk_status_message = get_unraid_disk_status($unraid_disk_status_color, "DISK_OK");
-					$unraid_add_greenblinkid = "";
-				}
-				else if(strstr($smart_powermode, "IDLE")) {
-					$unraid_disk_status_color = "green-on";
-					$unraid_disk_status_message = get_unraid_disk_status($unraid_disk_status_color, "DISK_OK");
-					$unraid_add_greenblinkid = "";
-				}
-				else if(strstr($smart_powermode, "STANDBY")) {
-					$unraid_disk_status_color = "green-blink";
-					$unraid_disk_status_message = get_unraid_disk_status($unraid_disk_status_color, "DISK_OK");
-					$unraid_add_greenblinkid = " class=\"greenblink\" id=\"greenblink\"";
-				}
-				else {
-					$unraid_disk_status_color = "grey-off";
-					$unraid_disk_status_message = get_unraid_disk_status($unraid_disk_status_color, "DISK_NP");
-					$unraid_add_greenblinkid = "";
-				}
-				
-				$unraid_array_icon = "<span class=\"" . $unraid_disk_status_color . "\" alt=\"" . $unraid_disk_status_message . "\" title=\"" . $unraid_disk_status_message . "\" />&#11044;</span>";
 			}
 			
-			switch($smart_status) {
-				case 1:
-					$smart_status_icon = "<span class=\"green-on\" alt=\"S.M.A.R.T: Passed\" title=\"S.M.A.R.T: Passed\" />&#11044;</span>";
-					break;
-				case 0:
-					$smart_status_icon = "<span class=\"red-on\" alt=\"S.M.A.R.T: Failed!\" title=\"S.M.A.R.T: Failed!\" />&#11044;</span>";
-					break;
-				default:
-					$smart_status_icon = "<span class=\"grey-off\" alt=\"S.M.A.R.T: Off/None\" title=\"S.M.A.R.T: Off/None\" />&#11044;</span>";
+			if($displayinfo["ledsmart"]) {
+				$smart_status = $data["smart_status"];
+				switch($smart_status) {
+					case 1:
+						$smart_status_icon = "<span class=\"green-on\" alt=\"S.M.A.R.T: Passed\" title=\"S.M.A.R.T: Passed\" />&#11044;</span>";
+						break;
+					case 0:
+						$smart_status_icon = "<span class=\"red-on\" alt=\"S.M.A.R.T: Failed!\" title=\"S.M.A.R.T: Failed!\" />&#11044;</span>";
+						break;
+					default:
+						$smart_status_icon = "<span class=\"grey-off\" alt=\"S.M.A.R.T: Off/None\" title=\"S.M.A.R.T: Off/None\" />&#11044;</span>";
+				}
 			}
 			
-			$unraid_dev = ( isset($unraid_array[$devicenode]["type"]) ? "<b>" . $unraid_array[$devicenode]["type"] . "</b>: " . $unraid_array[$devicenode]["name"] : "<b>Unassigned:</b>" );
+			if($displayinfo["unraidinfo"]) {
+				$unraid_dev = ( isset($unraid_array[$devicenode]["type"]) ? "<b>" . $unraid_array[$devicenode]["type"] . "</b>: " . $unraid_array[$devicenode]["name"] : "<b>Unassigned:</b>" );
+			}
 			
+			$tray_assign = ( empty($data["tray"]) ? $i : $data["tray"] );
 			$drive_tray_order[$luname] = get_tray_location($db, $luname);
 			$drive_tray_order[$luname] = ( empty($drive_tray_order[$luname]) ? $count_real : $drive_tray_order[$luname] );
+			if($displayinfo["tray"]) {
+				$add_traynumber = "<b>" . $drive_tray_order[$luname] . "</b>" . $insert_break . "";
+			}
 			
-			$devicenode_page = str_replace("-", "", $devicenode);
+			if($displayinfo["devicenode"]) {
+				$devicenode_page = str_replace("-", "", $devicenode);
+			}
+			
+			$add_break_1 = "";
+			$add_break_2 = "";
+			if($unraid_dev || $device_page || $devicenode_page || $luname_page) {
+				$add_break_1 = "<br />";
+			}
+			if($smart_modelfamily || $smart_modelname || $smart_serialnumber) {
+				$add_break_2 = "<br />";
+			}
 			
 			$disklocation_page .= "
 				<div style=\"order: " . $drive_tray_order[$luname] . "\">
 					<div class=\"flex-container\">
 						<div style=\"background-color: #" . $color_array[$luname] . ";\">
 							<div class=\"flex-container-start\">
-								<b>" . $drive_tray_order[$luname] . "</b>" . $insert_break . "
-								" . $unraid_array_icon . "" . $insert_break . "
-								" . $smart_status_icon . "
+								$add_traynumber
+								$unraid_array_icon
+								$smart_status_icon
 								
 							</div>
 							<div class=\"flex-container-middle\">
-								$unraid_dev $device $devicenode_page (" . $luname . ")<br />
-								$smart_modelfamily $smart_modelname <span style=\"white-space: nowrap;\">$smart_serialnumber</span><br />
+								$unraid_dev $device_page $devicenode_page $luname_page $add_break_1
+								$smart_modelfamily $smart_modelname $smart_serialnumber $add_break_2
 								$smart_temperature $smart_powerontime $smart_loadcycle $smart_capacity $smart_rotation $smart_formfactor
 							</div>
 							<!--
---
+++ disklocation/pages/sqlite_tables.php
@@ -0,0 +1,148 @@
+<?php
+/*
+	Database Version: current
+	
+	
+	Variable name		Default value	Description
+	--------------------------------------------------------------------------------
+	smart_exec_delay	'200',		set milliseconds for next execution for SMART shell_exec - needed to actually grab all the information for unassigned devices. Default: 200
+	bgcolor_unraid		'ef6441',	background color for Unraid array disks
+	bgcolor_others		'41b5ef',	background color for unassigned/other disks
+	bgcolor_empty		'aaaaaa',	background color for empty trays
+	grid_count		'column',	how to count the trays: [column]: trays ordered from top to bottom from left to right | [row]: ..from left to right from top to bottom
+	grid_columns		'4',		number of horizontal trays
+	grid_rows		'6',		number of verical trays
+	grid_trays		'',		total number of trays. default this is (grid_columns * grid_rows), but we choose to add some flexibility for drives outside normal trays
+	disk_tray_direction	'h',		direction of the hard drive trays [h]horizontal | [v]ertical
+	tray_width		'400',		the pixel width of the hard drive tray: in the horizontal direction ===
+	tray_height		'70'		the pixel height of the hard drive tray: in the horizontal direction ===
+	warranty_field		'u'		choose [u]nraid's way of entering warranty date (12/24/36... months) or enter [m]anual ISO dates.
+	tempunit		'C'		choose default temperature unit to display, default is [C]elsius (as reported from harddisks), other options: [F]arenheit | [K]elvin.
+	displayinfo		Array		This will store an json_encoded array for display settings for the "Device" page.
+
+*/
+$sql_create_disks = "
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	device VARCHAR(16) NOT NULL,
+	devicenode VARCHAR(8),
+	luname VARCHAR(50) UNIQUE NOT NULL,
+	model_family VARCHAR(50),
+	model_name VARCHAR(50),
+	smart_status TINYINT,
+	smart_serialnumber VARCHAR(128),
+	smart_temperature DECIMAL(4,1),
+	smart_powerontime INT,
+	smart_loadcycle INT,
+	smart_capacity INT,
+	smart_rotation INT,
+	smart_formfactor VARCHAR(16),
+	status CHAR(1),
+	purchased DATE,
+	warranty SMALLINT,
+	warranty_date DATE,
+	comment VARCHAR(255)
+";
+$sql_tables_disks = "
+	id,
+	device,
+	devicenode,
+	luname,
+	model_family,
+	model_name,
+	smart_status,
+	smart_serialnumber,
+	smart_temperature,
+	smart_powerontime,
+	smart_loadcycle,
+	smart_capacity,
+	smart_rotation,
+	smart_formfactor,
+	status,
+	purchased,
+	warranty,
+	warranty_date,
+	comment
+";
+$sql_create_location = "
+	id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	luname VARCHAR(50) UNIQUE NOT NULL,
+	empty VARCHAR(255),
+	tray SMALLINT
+";
+$sql_tables_location = "
+	id,
+	luname,
+	empty,
+	tray
+";
+$sql_create_settings = "
+	smart_exec_delay INT NOT NULL DEFAULT '200',
+	bgcolor_unraid CHAR(6) NOT NULL DEFAULT 'ef6441',
+	bgcolor_others CHAR(6) NOT NULL DEFAULT '41b5ef',
+	bgcolor_empty CHAR(6) NOT NULL DEFAULT 'aaaaaa',
+	grid_count VARCHAR(6) NOT NULL DEFAULT 'column',
+	grid_columns TINYINT NOT NULL DEFAULT '4',
+	grid_rows TINYINT NOT NULL DEFAULT '6',
+	grid_trays SMALLINT,
+	disk_tray_direction CHAR(1) NOT NULL DEFAULT 'h',
+	tray_width SMALLINT NOT NULL DEFAULT '400',
+	tray_height SMALLINT NOT NULL DEFAULT '70',
+	warranty_field CHAR(1) NOT NULL DEFAULT 'u',
+	tempunit CHAR(1) NOT NULL DEFAULT 'C',
+	displayinfo VARCHAR(1023)
+";
+$sql_tables_settings = "
+	smart_exec_delay,
+	bgcolor_unraid,
+	bgcolor_others,
+	bgcolor_empty,
+	grid_count,
+	grid_columns,
+	grid_rows,
+	grid_trays,
+	disk_tray_direction,
+	tray_width,
+	tray_height,
+	warranty_field,
+	tempunit,
+	displayinfo
+";
+
+/*
+	Database Version: 0
+*/
+
+$sql_tables_disks_v0 = "
+	id,
+	device,
+	devicenode,
+	luname,
+	model_family,
+	model_name,
+	smart_status,
+	smart_serialnumber,
+	smart_temperature,
+	smart_powerontime,
+	smart_loadcycle,
+	smart_capacity,
+	smart_rotation,
+	smart_formfactor,
+	status,
+	purchased,
+	warranty,
+	comment
+";
+$sql_tables_settings_v0 = "
+	smart_exec_delay,
+	bgcolor_unraid,
+	bgcolor_others,
+	bgcolor_empty,
+	grid_count,
+	grid_columns,
+	grid_rows,
+	grid_trays,
+	disk_tray_direction,
+	tray_width,
+	tray_height
+";
+?>
