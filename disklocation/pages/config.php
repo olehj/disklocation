495a86c67ae109a608677aa8e894a3bcadf9c44d
Ole-Henrik Jakobsen <ole@gidderikke.no>
2019-01-31 00:41:48 +0100

    Add files via upload
    Fixes #29 Updated javascript execution with a short delay after kill command

--- disklocation/pages/config.php
+++ disklocation/pages/config.php
@@ -318,15 +318,20 @@
 	</table>
 </form>
 <script>
-$('.diskLocation').click(function(e) {
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+$('.diskLocation').click(async function(e) {
 	var locateDisk = document.getElementById(this.id);
 	
 	if(locateDisk.value == "Locate") {
 		locateKillAll(locateDisk.className);
+		await sleep(200);
 		locateStart(locateDisk);
 	}
 	else {
 		locateStop(locateDisk);
+		await sleep(200);
 		locateKillAll(locateDisk.className);
 	}
 });
