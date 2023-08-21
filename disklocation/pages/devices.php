ae0e8ec97d3dad03b257954e385579ec5bfaaf1a
FlamongOle <ole@gidderikke.no>
2023-08-21 21:34:52 +0200

    Fixes #228

--- disklocation/pages/devices.php
+++ disklocation/pages/devices.php
@@ -299,7 +299,7 @@
 					$device_comment = ( !isset($data["comment"]) ? null : bscode2html(stripslashes(htmlspecialchars($data["comment"]))) );
 				}
 				if(isset($displayinfo["temperature"]) || isset($displayinfo["ledtemp"])) {
-					if(is_float($unraid_array[$data["devicenode"]]["temp"]) && is_float($unraid_array[$devicenode]["temp"])) {
+					if(is_numeric($unraid_array[$data["devicenode"]]["temp"]) && is_numeric($unraid_array[$devicenode]["temp"])) {
 						switch($display["unit"]) {
 							case 'F':
 								$smart_temperature = round(temperature_conv($unraid_array[$data["devicenode"]]["temp"], 'C', 'F')) . "°F";
