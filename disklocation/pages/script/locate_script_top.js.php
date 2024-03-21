168aea0eb9e6a87d721784525f9161b1247a1c95
FlamongOle <ole@gidderikke.no>
2024-03-21 09:39:59 +0100

    Fixes #307

--- disklocation/pages/script/locate_script_top.js.php
+++ disklocation/pages/script/locate_script_top.js.php
@@ -48,7 +48,7 @@ function locateKillAll(locateDisk){
 	for (i = 0; i < y.length - 1; i++) {
 		y[i].value = "Locate";
 		y[i].style.backgroundColor = "#F2F2F2";
-		//document.getElementById("bg3-" + y[i].id).classList.remove('flex-container-locate');
+		document.getElementById("bg3-" + y[i].id).classList.remove('flex-container-locate');
 	}
 	$.get('<?php echo $path ?>',{ cmd:"killall"},function(data) {});
 }
